adce14a3aca7b898e1fe9e0178fd3ca7c4097869
dean.ryan <L00188387@atu.ie>
2024-11-25 10:39:21 +0000

    feat(main-application-deployment) configure all the stacks to be able to deploy infrastructure #15
    Updated Readme file with how to for deployment actions and destory
    
    added providers for AWS for use in stacks #2
    
    configured main.py to use my custom stacks for network and instance #15 #5 #4
    
    cleaned up stacks and variables #4 #5 #8

--- stacks/InstanceStack.py
+++ stacks/InstanceStack.py
@@ -1,52 +1,62 @@
 from cdktf import TerraformStack
 from constructs import Construct
-from imports.aws import Instance, SecurityGroup
+from imports.aws.security_group import SecurityGroup
+from imports.aws.vpc_security_group_egress_rule import VpcSecurityGroupEgressRule
+from imports.aws.vpc_security_group_ingress_rule import VpcSecurityGroupIngressRule
+from imports.aws.instance import Instance
 from variables import NetworkConfig
+from providers import create_aws_provider
 
 class InstanceStack(TerraformStack):
-    def __init__(self, scope: Construct, id: str, config: NetworkConfig, NetworkStack):
+    def __init__(self, scope: Construct, id: str, config: NetworkConfig, network_stack):
         super().__init__(scope, id)
         
-        #security group
-        securityGroup = SecurityGroup(self, "security-group",
-            name=f"{config.project_name}-security-group",
-            vpc_id=NetworkStack.vpc_id,         
-            ingress=[{
-                "protocol": "tcp",
-                "from_port": 443,
-                "to_port": 443,
-                "cidr_blocks": ["0.0.0.0/0"],
-                "description": "HTTPS"
-            }, 
-            {
-                "protocol": "tcp",
-                "from_port": 22,
-                "to_port": 22,
-                "cidr_blocks": ["10.0.1.0/24"],
-                "description": "SSH"
-            }],       
-            egress=[{
-                "protocol": "-1",
-                "from_port": 0,
-                "to_port": 0,
-                "cidr_blocks": ["0.0.0.0/0"],
-                "description": "All outbound traffic"
-            }],
-            
+        # Create AWS Provider
+        create_aws_provider(self, id)
+
+        # Default security group for instance
+        webSecurityGroup = SecurityGroup(self, "WebSG",
+            description="Allow inbound traffic and all outbound traffic",
+            name="securityGroup",
             tags={
-                "Name": f"{config.project_name}-security-group",
-                "Environment": config.environment
-            }
+                "Name": "Allow inbound traffic and all outbound traffic"
+            },
+            vpc_id=network_stack.vpc_id,
+        )
+
+        # Allow All
+        VpcSecurityGroupEgressRule(self, "allow_all_traffic_inbound_ipv4",
+            cidr_ipv4="0.0.0.0/0",
+            ip_protocol="-1",
+            security_group_id=webSecurityGroup.id
+        )
+
+         #Allow HHTPS
+        VpcSecurityGroupIngressRule(self, "allow_https_ipv4",
+            cidr_ipv4="0.0.0.0/0",
+            from_port=443,
+            ip_protocol="tcp",
+            security_group_id=webSecurityGroup.id,
+            to_port=443
+        )
+
+        # Allow SSH
+        VpcSecurityGroupIngressRule(self, "allow_ssh_ipv4",
+            cidr_ipv4="0.0.0.0/0",
+            from_port=22,
+            ip_protocol="tcp",
+            security_group_id=webSecurityGroup.id,
+            to_port=22
         )
 
         # EC2 Instance
-        Instance(self, "Public-Instance",
+        Instance(self, "WebServer",
             ami="ami-00385a401487aefa4",
             instance_type="t2.micro",
-            subnet_id=NetworkStack.publicSubnetCidr,
-            vpc_security_group_ids=[securityGroup.id],
+            subnet_id=network_stack.public_subnet_id,
+            vpc_security_group_ids=[webSecurityGroup.id],
             tags={
-                "Name": f"{config.project_name}-web-server",
+                "Name": f"{config.projectName}-web-server",
                 "Environment": config.environment
             }
         )
--- variables.py
+++ variables.py
@@ -2,9 +2,9 @@ from dataclasses import dataclass
 
 @dataclass
 class NetworkConfig:
-    vpc_cidr: str = "10.0.0.0/16"
+    vpcCidr: str = "10.0.0.0/16"
     publicSubnetCidr: str = "10.0.1.0/24"
     privateSubnetCidr: str = "10.0.2.0/24"
     availability_zone: str = "eu-west-1"
     environment: str = "dev"
-    project_name: str = "Lab2-folio-CDKTF-AWS"
+    projectName: str = "Lab2-folio-CDKTF-AWS"
